f5687a4556eeb2756f8382772f7489fad973063b
Dakota Lightning <im@dakotalightning.com>
2015-12-22 14:59:51 -0800

    markdown fix

--- web/app/helpers/application_helper.rb
+++ web/app/helpers/application_helper.rb
@@ -9,8 +9,12 @@ module ApplicationHelper
   end
 
   # markdown helper, usage <%= markdown(@blog.body) %>
-  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, no_intra_emphasis: true, fenced_code_blocks: true, disable_indented_code_blocks: true)
-    markdown.render(text).html_safe
+  def markdown(text)
+    Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+      no_intra_emphasis: true,
+      fenced_code_blocks: true,
+      disable_indented_code_blocks: true)
+        markdown.render(text).html_safe
   end
 
 end
